89cb0e92401f812f10a81d824f5e6adcac210d5c
Yuri Geovani <yuri_geovani@hotmail.com>
2023-06-05 01:22:41 +0100

    until adicionarAluno

--- Semestre 1/VIV0001 - INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/Avaliação NF/tema1/main.c
+++ Semestre 1/VIV0001 - INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/Avaliação NF/tema1/main.c
@@ -1,61 +1,84 @@
 #include <stdio.h>
-// #include <stdlib.h>
 
 struct Aluno
 {
     int matricula;
     char *nome;
+    int idade;
+    char genero; // m or f or o
+    float media;
 };
 
-// void listarOpcoes()
-// {
-// }
+void adicionarAluno (){
+    struct Aluno aluno;
+    printf("MATRICULA: ");
+    scanf("%i", &aluno.matricula);
+    printf("NOME: ");
+    fgets(aluno.nome, sizeof(aluno.nome), stdin);  // read string
+    // scanf(" %s", aluno.nome);
+    printf("IDADE: ");
+    scanf("%i", &aluno.idade);
+    printf("GENERO: ");
+    scanf("%c", &aluno.genero);
+    printf("MEDIA: ");
+    scanf("%f", &aluno.media);
+    
+    printf("\nALUNO CADASTRADO COM SUCESSO!\n");
+    printf("\tMATRICULA: %i\n", aluno.matricula);
+    printf("\tNOME: %s\n", aluno.nome);
+    printf("\tIDADE: %i\n", aluno.idade);
+    printf("\tGENERO: %c\n", aluno.genero);
+    printf("\tMEDIA: %f\n", aluno.media);
+}
 
-// char validarOpcao(int opcao){
-//     if (isdigit(opcao)){
-//         printf("entrou");
-//         return opcao;
-//     }
-// }
+void listarOpcoes()
+{
+    printf("\n############## ESTACIO ACADEMY ##############");
+    printf("\nInforme a opcao desejada:");
+    printf("\n1) Cadastro de alunos");
+    printf("\n2) Listagem de alunos");
+    printf("\n3) Calculo de media");
+    printf("\n4) Pesquisa de alunos");
+    printf("\n5) Atualizacao de dados");
+    printf("\n6) Remocao de alunos");
+    printf("\n0) Sair");
+}
+
+char digitarOpcao (char opcao){
+    printf("\nOpcao: ");
+    scanf(" %c", &opcao);
+    return opcao;
+}
+
+char selecionarOpcao(char opcao) {
+    switch (opcao) {
+    case 49:
+        adicionarAluno();
+        break;
+    case 50:
+        // printf("%i %s\n\n", aluno.matricula, aluno.nome);
+        break;
+    case 51:
+        printf("\nop3");
+        break;
+    case 48:
+        break;
+    default:
+        printf("\nSelecione uma opcao correta!!!");
+        break;
+    }
+}
 
 int main()
 {
-    struct Aluno a1;
+    // struct Aluno aluno;
     char opcao;
 
     while (opcao!=48)
     {
-        printf("\n############## ESTACIO ACADEMY ##############");
-        printf("\nInforme a opcao desejada:");
-        printf("\n1) Cadastro de alunos");
-        printf("\n2) Listagem de alunos");
-        printf("\n3) Calculo de media");
-        printf("\n4) Pesquisa de alunos");
-        printf("\n5) Atualizacao de dados");
-        printf("\n6) Remocao de alunos");
-        printf("\n0) Sair");
-        printf("\nOpcao: ");
-        scanf(" %c", &opcao);
-
-        switch (opcao)
-        {
-        case 49:
-            a1.matricula = 123;
-            a1.nome = "Tests";
-            printf("\nUSUARIO CADASTRADO!\n\n");
-            break;
-        case 50:
-            printf("\n%i %s", a1.matricula, a1.nome);
-            break;
-        case 51:
-            printf("\nop3");
-            break;
-        case 48:
-            break;
-        default:
-            printf("\nSelecione uma opcao correta!!!");
-            break;
-        }
+        listarOpcoes();
+        opcao = digitarOpcao(opcao);
+        selecionarOpcao(opcao);
     }
     return 0;
 }
