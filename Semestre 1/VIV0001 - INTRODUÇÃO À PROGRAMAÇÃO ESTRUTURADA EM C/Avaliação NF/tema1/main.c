f740c302998e40b71919268b710fc3a3b5f7eeff
Yuri Geovani <yuri_geovani@hotmail.com>
2023-06-12 17:43:57 +0100

    Completed work

--- Semestre 1/VIV0001 - INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/Avaliação NF/tema1/main.c
+++ Semestre 1/VIV0001 - INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/Avaliação NF/tema1/main.c
@@ -57,6 +57,70 @@ Aluno *criarAluno(Aluno *alunos){
             printf("\nEntrada invalida! Informe uma media válida.\n");
     }
 
+    printf("\nAluno cadastrado!\n");
+    return alunos;
+}
+
+Aluno *updateAlunoByMatricula(Aluno *alunos){
+    char temp;
+    int i, matricula;
+    printf("\nInforme a matricula: ");
+    scanf("%d", &matricula);
+
+    i = matricula-1;
+        scanf("%c", &temp); // instrucao temporaria pra limpar o buffer apos int e ENTER
+    printf("NOME: ");
+    fgets(alunos[i].nome, sizeof(alunos[i].nome), stdin);
+        alunos[i].nome[strlen(alunos[i].nome)-1] = '\0'; // remove o \n do ultimo valor do array, dado pelo fgets
+
+    alunos[i].idade = -1;
+    while (alunos[i].idade <= 0 || alunos[i].idade > 120)
+    {
+        printf("IDADE: ");
+        scanf("%d", &alunos[i].idade);
+
+        if(alunos[i].idade <= 0 || alunos[i].idade > 120)
+            printf("\nEntrada invalida! Informe uma idade válida.\n");
+    }
+
+    alunos[i].genero = -1;
+    while (alunos[i].genero != 70 && alunos[i].genero != 77 && 
+            alunos[i].genero != 102 && alunos[i].genero != 109 && 
+            alunos[i].genero != 111 && alunos[i].genero != 79)
+    {
+        printf("GENERO (m/f/o): ");
+        scanf(" %c", &alunos[i].genero);
+
+        if(alunos[i].genero != 70 && alunos[i].genero != 77 && 
+            alunos[i].genero != 102 && alunos[i].genero != 109 && 
+            alunos[i].genero != 111 && alunos[i].genero != 79)
+            printf("\nEntrada invalida! Informe M para masculino, F para feminino, O para outros.\n");
+    }
+
+    alunos[i].media = -1;
+    while (alunos[i].media < 0 || alunos[i].media > 10)
+    {
+        printf("MEDIA: ");
+        scanf("%f", &alunos[i].media);
+
+        if(alunos[i].media < 0 || alunos[i].media > 10)
+            printf("\nEntrada invalida! Informe uma media válida.\n");
+    }
+
+    printf("\nAluno atualizado!\n");
+    return alunos;
+}
+
+Aluno *deleteAlunoByMatricula(Aluno *alunos){
+    int i, matricula;
+    printf("\nInforme a matricula: ");
+    scanf("%d", &matricula);
+
+    i = matricula-1;
+
+    strcpy(alunos[i].nome,"DESATIVADO");
+
+    printf("\nAluno removido!\n");
     return alunos;
 }
 
@@ -65,20 +129,56 @@ void listAllAlunos (Aluno *alunos){
     printf("\n############## ALUNOS ##############");
     while (alunos[i].matricula)
     {
+        if(strcmp(alunos[i].nome,"DESATIVADO"))
+        {
+            printf("\nMATRICULA: %i", alunos[i].matricula);
+            printf("\nNOME: %s", alunos[i].nome);
+            printf("\nIDADE: %i", alunos[i].idade);
+            if (alunos[i].genero==109 || alunos[i].genero==77)
+            {
+                printf("\nGENERO: Masculino");
+            } else if (alunos[i].genero==102 || alunos[i].genero==70)
+            {
+                printf("\nGENERO: Feminino");
+            } else {
+                printf("\nGENERO: Outros");
+            }
+            printf("\nMEDIA: %.2f\n", alunos[i].media);
+        }
+        i++;
+    }
+}
+
+void listAlunoByMatricula (Aluno *alunos){
+    int i, matricula;
+    printf("\nInforme a matricula: ");
+    scanf("%d", &matricula);
+
+    while (alunos[i].matricula && alunos[i].matricula != matricula)
+    {
+        i++;
+    }
+
+    if (matricula == 0 || alunos[i].matricula != matricula) {
+        printf("\nMatricula nao encontrada!\n");
+    } else if (alunos[i].matricula == matricula)
+    {
         printf("\nMATRICULA: %i", alunos[i].matricula);
         printf("\nNOME: %s", alunos[i].nome);
         printf("\nIDADE: %i", alunos[i].idade);
-        if (alunos[i].genero==109 || alunos[i].genero==77)
+        if (alunos[i].genero == 109 || alunos[i].genero == 77)
         {
             printf("\nGENERO: Masculino");
-        } else if (alunos[i].genero==102 || alunos[i].genero==70)
+        }
+        else if (alunos[i].genero == 102 || alunos[i].genero == 70)
         {
             printf("\nGENERO: Feminino");
-        } else {
+        }
+        else
+        {
             printf("\nGENERO: Outros");
         }
         printf("\nMEDIA: %.2f\n", alunos[i].media);
-        i++;
     }
 }
 
@@ -105,9 +205,9 @@ void listarOpcoes()
     printf("\n1) Cadastro de alunos");
     printf("\n2) Listagem de alunos");
     printf("\n3) Calculo de media");
-    printf("\n4) Pesquisa de alunos");
+    printf("\n4) Pesquisa de alunos por matricula");
     printf("\n5) Atualizacao de dados");
-    printf("\n6) Remocao de alunos");
+    printf("\n6) Remover aluno por matricula");
     printf("\n0) Sair");
 }
 
@@ -129,13 +229,13 @@ void selecionarOpcao(char opcao, Aluno *alunos) {
         mediaGeral(alunos);
         break;
     case 52:
-        printf("\nPesquisa por matricula");
+        listAlunoByMatricula(alunos);
         break;
     case 53:
-        printf("\nUpdate");
+        alunos = updateAlunoByMatricula(alunos);
         break;
     case 54:
-        printf("\nDelete");
+        alunos = deleteAlunoByMatricula(alunos);
         break;
     case 48:
         break;
