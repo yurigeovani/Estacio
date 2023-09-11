f264bd60411d883ea5fd49b82bdfc5ea28ac3407
Yuri Geovani <yuri_geovani@hotmail.com>
2023-09-11 17:58:00 +0100

    Until exercise 10

--- Semestre 2/VIV0065 - ESTRUTURA DE DADOS EM C/0910 - Atividade/10.c
+++ Semestre 2/VIV0065 - ESTRUTURA DE DADOS EM C/0910 - Atividade/10.c
@@ -2,6 +2,23 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    /* 10. Crie um programa que contenha um vetor contendo 5 elementos inteiros.
+    Leia esse vetor do teclado e imprima o endereço das posições contendo valores pares. */
+
+    int vetor[5];
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("Informe o valor na posicao %i = ", i);
+        scanf("%i", &vetor[i]);
+    }
+
+     for (int i = 0; i < 5; i++) {
+        if (vetor[i]%2==0)
+        {
+            printf("Posicao: %i, endereco: %p, valor: %d\n", i, &vetor[i], vetor[i]);
+        }   
+    }
+
     return 0;
 }
