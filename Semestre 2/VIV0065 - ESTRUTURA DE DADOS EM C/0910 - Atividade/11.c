62d4046fdf7d6d42f444e13f148b6aeb503822fc
Yuri Geovani <yuri_geovani@hotmail.com>
2023-09-12 02:23:27 +0100

    Until exercise 12

--- Semestre 2/VIV0065 - ESTRUTURA DE DADOS EM C/0910 - Atividade/11.c
+++ Semestre 2/VIV0065 - ESTRUTURA DE DADOS EM C/0910 - Atividade/11.c
@@ -1,7 +1,50 @@
 #include<stdio.h>
 
+    /*  Faça um programa que leia três valores inteiros e chame uma função que receba esses 
+    3 valores de entrada e os retorne ordenados, ou seja, o menor valor na primeira variável, 
+    o segundo menor valor na variável do meio e o maior valor na última variável. A função 
+    deverá retornar o valor 1 se os três valores forem iguais e 0 se existirem valores 
+    diferentes. Exiba os valores ordenados na tela. */
+
+int ordenaValores (int numeros[3]) {
+    int temp;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (numeros[i]<numeros[j])
+            {
+                temp = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = temp;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        printf("O valor na posicao %li eh = %i\n", i, numeros[i]);
+    }
+
+    if(numeros[0]==numeros[1] && numeros[0]==numeros[2])
+        return 1;
+    else
+        return 0;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    int numeros[3];
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        printf("Informe o valor do numero %li = ", i);
+        scanf("%i", &numeros[i]);
+    }
+
+    printf("Retorno da funcao = %i\n", ordenaValores(numeros));
+
     return 0;
 }
--- Semestre 2/VIV0065 - ESTRUTURA DE DADOS EM C/0910 - Atividade/12.c
+++ Semestre 2/VIV0065 - ESTRUTURA DE DADOS EM C/0910 - Atividade/12.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
+#include<string.h>
+
+    /* Elabore uma função que receba duas strings como parâmetros e verifique se a segunda 
+    string ocorre dentro da primeira. Use aritmética de ponteiros para acessar os caracteres 
+    das strings. */
+
+//char *strstr(const char *haystack, const char *needle)
+void *procuraString (char *s1, char *s2){
+    if (strstr(s1, s2))
+        printf("String encontrada: %s\n", s2);
+    else
+        printf("String nao encontrada.\n");
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    char *s1 = "Teste";
+    char *s2 = "Te";
+
+    procuraString(s1, s2);
     return 0;
 }
